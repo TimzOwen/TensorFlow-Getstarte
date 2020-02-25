00a20ec1a8f5c7503c9c6b74f737819d0505fe24
Timz Owen <ktimothy@kabarak.ac.ke>
2020-02-25 23:50:25 +0300

    Create house-seller-Tensor.py
    boston initial data sets

---
+++ house-seller-Tensor.py
@@ -0,0 +1,13 @@
+// import the required libraries
+import numpy as np 
+import matplotlip.pyplot as plt
+import pandas an pd
+import sklearn 
+from sklearn.datasets import load_boston
+
+#import the data set 
+boston = laod_boston()
+
+bos = pd.DataFrame(boston.Data)
+
+print(bos.head(10))
