3ddc0434fe4367a05987078691839d4203c671c4
Timz Owen <ktimothy@kabarak.ac.ke>
2020-03-29 23:48:02 +0300

    Update basics.py
    variables and graphs

--- basics.py
+++ basics.py
@@ -1,1 +1,19 @@
+# Tensorflow basics with varibles and constant used to store data and compute graphs
+x= tf.constant(40, name="x")
+y=tf.Variable(x+20, name="y")
+model=tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(model)
+    print(sess.run(y)) #60
+    
+#arrays 
 
+x = tf.constant([35, 40, 45], name='x')
+y = tf.Variable(x + 5, name='y')
+
+
+model = tf.global_variables_initializer()
+
+with tf.Session() as session:
+	session.run(model)
+	print(session.run(y)) #40,45,50
