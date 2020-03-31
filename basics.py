1c4c7c1ede03e433e1b7619fe4b7a54dcbae23ff
Timz Owen <ktimothy@kabarak.ac.ke>
2020-03-31 22:29:29 +0300

    Update basics.py
    reverse_sequence

--- basics.py
+++ basics.py
@@ -88,3 +88,29 @@ with tf.Session() as sess:
     
 plt.imshow(image)
 plt.show()  # output on readme.ME transpose
+
+# Reverse manipulation using (reverse_sequence)
+import numpy as np
+import tensorflow as tf
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import os
+# First, load the image again
+dir_path = os.path.dirname(os.path.realpath(__file__))
+filename = dir_path + "/picturename.jpg"
+image = mpimg.imread(filename)
+height, width, depth = image.shape
+
+# Create a TensorFlow Variable
+x = tf.Variable(image, name='x')
+
+model = tf.global_variables_initializer()
+
+with tf.Session() as session:
+    x = tf.reverse_sequence(x, [width] * height, 1, batch_dim=0)
+    session.run(model)
+    result = session.run(x)
+
+print(result.shape)
+plt.imshow(result)
+plt.show()
