bc7c7e93dfcf911f3f601d8a691c5633cfdbaa9a
Timz Owen <ktimothy@kabarak.ac.ke>
2020-04-03 22:40:10 +0300

    Update basics.py
    3d and rotations

--- basics.py
+++ basics.py
@@ -175,3 +175,44 @@ z.eval()
 #print resource but be sure your computer is powerful otherwise don't run (4gb Ram +)
 
 #upnext Tensorfow in 3D
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+from matplotlib import cm
+import matplotlib.pyplot as plt
+from scipy.spatial import Delaunay
+​
+def plot_basic_object(points):
+    """Plots a basic object, assuming its convex and not too complex"""
+    tri = Delaunay(points).convex_hull
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    S = ax.plot_trisurf(points[:,0], points[:,1], points[:,2],
+                        triangles=tri,
+                        shade=True, cmap=cm.Blues,lw=0.5)
+    ax.set_xlim3d(-5, 5)
+    ax.set_ylim3d(-5, 5)
+    ax.set_zlim3d(-5, 5)
+
+    plt.show()
+import numpy as np
+def create_cube(bottom_lower=(0, 0, 0), side_length=5):
+    """Creates a cube starting from the given bottom-lower point (lowest x, y, z values)"""
+    bottom_lower = np.array(bottom_lower)
+    points = np.vstack([
+        bottom_lower,
+        bottom_lower + [0, side_length, 0],
+        bottom_lower + [side_length, side_length, 0],
+        bottom_lower + [side_length, 0, 0],
+        bottom_lower + [0, 0, side_length],
+        bottom_lower + [0, side_length, side_length],
+        bottom_lower + [side_length, side_length, side_length],
+        bottom_lower + [side_length, 0, side_length],
+        bottom_lower,
+    ])
+    return points
+cube_1 = create_cube(side_length=2)
+​
+​
+plot_basic_object(cube_1)
+
+#gives the output as on readme image
