6b099772bb38044a66757e80c7660a2610d69afd
Timz Owen <ktimothy@kabarak.ac.ke>
2020-04-20 23:12:37 +0300

    Update Computationgraphsm.py
    linear regression

--- Computationgraphsm.py
+++ Computationgraphsm.py
@@ -172,3 +172,45 @@ print(y.eval(feed_dict={[5]})) # prints[20]
 
 sess.close()
 
+          
+   #LINEAR REGRESSION IN REALWORLD WITH TENSORFLOW
+import tensorflow as tf
+
+#Model parameters
+W = tf.Variable([.3], dtype='float32')
+b = tf.Variable([-.3], dtype='float32')
+
+# Model input and output
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+
+#output holder 
+y = tf.placeholder(tf.float32)
+
+#Loss model during training 
+loss = tf.reduce_mean(tf.square(linear_model, - y))
+
+#optimize your model for the best results and accuracy
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+
+#Training the model
+train = optimizer.minimize(loss)
+
+#Training data
+x_train = [1,2,3,4]
+y_train = [0,-1,-2,-3]
+
+#initialize all variables
+init = tf.global_variables_initializer()
+
+#instantiate sessions and train in loops of 1000
+with tf.Session() as sess:
+    sess.run(init)
+    for i in range(100):
+        sess.run(train, {x: x_train, y: y_train})
+        
+    #evaluate training accuracy 
+    curr_w, curr_b, curr_loss = sess.run([W, b, loss], {x: x_train, y: y_train})
+    
+    print("W: %s b: %s loss: %s" %s(curr_w, curr_b, curr_loss))
+
