9c1bfaefee24ab7a5dffd3f6c20e1125bf97c7ab
Timz Owen <ktimothy@kabarak.ac.ke>
2020-04-20 23:11:43 +0300

    Update Computationgraphsm.py
    interactive session

--- Computationgraphsm.py
+++ Computationgraphsm.py
@@ -152,3 +152,23 @@ with tf.Session() as sess:
     
     writer = tf.summary.FileWriter('./file_location', sess.graph)
     writer.close()
+          
+          
+    # INTERACTIVE SESSION()
+# allows one to run inferences without having to instantiate session each time
+
+import tensorflow as tf
+
+sess = tf.interactiveSession()
+
+A = tf.constant([4], tf.int32, name='a')
+x = tf.placeholder(tf.int32, name='x')
+
+y = A * x
+
+y.eval(feed_dict={x:[5]})  # array ([20]) dtype=int32
+
+print(y.eval(feed_dict={[5]})) # prints[20]
+
+sess.close()
+
