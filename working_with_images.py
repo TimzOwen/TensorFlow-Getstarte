ffa042f3ecc04b46e44deba3827f3b313348f3b8
Timz Owen <ktimothy@kabarak.ac.ke>
2020-04-22 23:05:14 +0300

    Update working_with_images.py
    transposing

--- working_with_images.py
+++ working_with_images.py
@@ -18,3 +18,42 @@ print("image Array", image)
 #show on the plot 
 plt.imshow(image)
 plt.show()
+
+#NOW RUN TRANSPOSING 
+#representing and transposing images
+import tensorflow as tf
+import matplotlib.image as mp_image
+import matplotlib.pyplot as plt
+import os
+
+#file location
+file_name = "./directory_image.png"
+
+#read image 
+image = mp_image.imread(file_name)
+
+#print array and shape
+print("image shape", image.shape)
+print("image Array", image)
+
+#show on the plot 
+plt.imshow(image)
+plt.show()
+
+#create a variable to hold the image
+x = tf.Variable(image, name='x')
+
+#initialize the variables
+init = tf.global_variables_initializer()
+
+with tf.Session as sess:
+    sess.run(init)
+    #swap axis indexes from 0,1,2 using transpose
+    transpose = tf.transpose(x, perm=[1,0,2]) #alternative: transpose = tf.image.transpose_image(x)
+    result = sess.run(transpose)
+    
+    #print shape and display using matplotlib
+    print("Transposed image:",result.shape)
+    plt.imshow(result)
+    plt.show()
+#run the above file on terminal and you see th transpose
