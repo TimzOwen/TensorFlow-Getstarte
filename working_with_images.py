bf5a062d6fb3e34dc12be67478b348262a8f2cb3
Timz Owen <ktimothy@kabarak.ac.ke>
2020-04-25 10:46:27 +0300

    Update working_with_images.py
    image resizing and reading

--- working_with_images.py
+++ working_with_images.py
@@ -122,3 +122,81 @@ with tf.Session() as sess:
         summary_writer.close()
         
         #now you can view the result from the above code on tensorboard
+        
+        
+        
+        
+        # REPRESENTING 4-D TENSOR IMAGES IN A LIST 
+        # Image Resizing and loading
+import tensorflow as tf
+from PIL import Image
+
+#list all the images in your directory
+original_image_list = ["./image1",
+                        "./image2",
+                        "./image3",
+                        "./image4"]
+
+#make a queue for all file names and images too
+file_queue = tf.train.string_input_producer(original_image_list)
+
+#read the entire image file
+image_reader = tf.wholeReader()
+
+# Session, coordinates and iteration
+with tf.Session() as sess:
+    #coordinate the loading of image files
+    coord = tf.train.Coordinator()
+    threads = tf.train_start_queue_runners(sess=sess, coord=coord)
+    
+    image_list = []
+    
+    # loop for resizing and shaping 
+    for i in range(len(original_image_list)):
+        #Read files from the queue and the first value returned is tuple
+        #ignore file names
+        _, image_file = image_reader.read(file_queue)
+        
+        #now decode the image ad JPEG  which is turned to a tensor ready for training 
+        image = tf.image.decode_jpeg(image_file)
+        
+        #get a resized tensor image
+        image = tf.image.resize_images(image,[250,250])
+        image.set_shape((250,250,3))
+        
+        #now print the value after getting image tensor
+        image_array = sess.run(image)
+        print(image_array.shape) 
+        
+        #convert numpy array to a tensor of type (250,250,3)
+        image_tensor = tf.stack(image_array)
+        
+        print(image_tensor)
+        image_list.append(image_tensor)
+        
+        #add a new Dimention using expands
+        image_list.append(tf.expand_dims(image_array,0))
+        
+        #finish the file name queues
+        coord.request_stop()
+        coord.join(threads)
+        
+       #covert all tensors to 4 -D tensor images
+       # 4-D tensor can be represented as (0,250,250,3) where 0 is the number of images 
+       image_tensor = tf.stack(image_tensor)
+       print(image_tensor)
+
+       # print out the summary on a tensorboard and get to see the images,
+       #to display the 4th image, add (max_outputs=4)
+       summary_writer = tf.summary.FileWriter('./directory', graph = sess.graph)
+    
+       #write all the images in one go
+       summary_str = sess.run(tf.summary.image("images", image_tensor))
+       summary_writer.add_summary(summary_str)
+        #close the writre and run the images and observe the changes on the tensorboard
+       summary_writer.close()
+        
+        
+        
+        
+        
