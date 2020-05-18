615bf35cdd2bafedce96844e66eadce2bdb6c27f
Timz Owen <ktimothy@kabarak.ac.ke>
2020-05-18 04:47:04 +0300

    Update introduction.py
    tf operation and function/images

--- introduction.py
+++ introduction.py
@@ -27,3 +27,58 @@ B1 = A1.numpy()
 
 # Print B1
 print(B1)
+
+
+#UPNEXT OPERATIONS
+# Define tensors A1 and A23 as constants
+A1 = constant([1, 2, 3, 4])
+A23 = constant([[1, 2, 3], [1, 6, 4]])
+
+# Define B1 and B23 to have the correct shape
+B1 = ones_like([2,3,4,5,])
+B23 = ones_like([[4,5,6,], [7,8,9]])
+
+# Perform element-wise multiplication
+C1 = multiply(A1,B1)
+C23 = multiply(A23,B23)
+
+# Print the tensors C1 and C23
+print('C1: {}'.format(C1.numpy()))
+print('C23: {}'.format(C23.numpy()))
+
+
+#making prediction with matrix multiplication
+# Define features, params, and bill as constants
+features = constant([[2, 24], [2, 26], [2, 57], [1, 37]])
+params = constant([[1000], [150]])
+bill = constant([[3913], [2682], [8617], [64400]])
+
+# Compute billpred using features and params
+billpred = matmul(features, params)
+
+# Compute and print the error
+error = bill - billpred
+print(error.numpy())
+
+#IMAGE TENSOR AND OPTIMISATION# Reshape the grayscale image tensor into a vector
+gray_vector = reshape(gray_tensor, (784, 1))
+
+# Reshape the color image tensor into a vector
+color_vector = reshape(color_tensor, (2352,1))
+
+
+#working with Gradient Function
+def compute_gradient(x0):
+      	# Define x as a variable with an initial value of x0
+	x = tf.Variable(x0)
+	with GradientTape() as tape:
+		tape.watch(x)
+        # Define y using the multiply operation
+		y = tf.multiply(x,x)
+    # Return the gradient of y with respect to x
+	return tape.gradient(y, x).numpy()
+
+# Compute and print gradients at x = -1, 1, and 0
+print(compute_gradient(-1.0))
+print(compute_gradient(1.0))
+print(compute_gradient(0.0))
