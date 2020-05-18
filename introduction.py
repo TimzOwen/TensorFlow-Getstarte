f91d532ee0a266c1a6bbf9f460aad4993c9b513b
Timz Owen <ktimothy@kabarak.ac.ke>
2020-05-18 17:12:25 +0300

    Update introduction.py
    Dense layers and neural networks

--- introduction.py
+++ introduction.py
@@ -343,3 +343,138 @@ for batch in pd.read_csv('kc_housing.csv', chunksize=100):
     
 #print the parameter values
 print(intercept.numpy(), slope.numpy())
+
+#output
+10.21788 , 0.7061
+
+
+
+#DENSE LAYERS
+#NEURAL NETWORKS 
+import tensorflow as tf
+
+#define inputs(features)
+inputs = tf.constant([[1,35]])
+
+#define weights
+weights = tf.Variable([[-0.05],[-0.01]])
+
+#Define the bias
+bias = tf.Variabes([0.5])
+
+#multiply inputes & weights (inputs*weights)
+product = tf.matmul(inputs, weights)
+
+#define the dense layer
+dense = tf.keras.activations.sigmoid(product + bias)
+
+
+#Defining a complete model
+import tensorflow as tf
+
+#define inputs
+inputs = tf.constant(data, tf.float32)
+
+#def first dense layer
+dense1 = tf.keras.layers.Dense(10, activation='sigmoid')(inputs)
+
+#define 2nd layer
+dense2 = tf.keras.layers.Dense(5, activation='sigmoid')(dense1)
+
+#define output prediction layer
+outputs = tf.keras.layers.Dense(1, activation='sigmoid')(dense2)
+
+
+#EXAMPLE 002 DENSE LAYERS
+
+# Initialize bias1
+bias1 = Variable(1.0)
+
+# Initialize weights1 as 3x2 variable of ones
+weights1 = Variable(ones((3, 2)))
+
+# Perform matrix multiplication of borrower_features and weights1
+product1 = matmul(borrower_features, weights1)
+
+# Apply sigmoid activation function to product1 + bias1
+dense1 = keras.activations.sigmoid(product1 + bias1)
+
+# Print shape of dense1
+print("\n dense1's output shape: {}".format(dense1.shape))
+
+#output
+dense1's output shape: (1, 2)
+
+# From previous step
+bias1 = Variable(1.0)
+weights1 = Variable(ones((3, 2)))
+product1 = matmul(borrower_features, weights1)
+dense1 = keras.activations.sigmoid(product1 + bias1)
+
+# Initialize bias2 and weights2
+bias2 = Variable(1.0)
+weights2 = Variable(ones((2, 1)))
+
+# Perform matrix multiplication of dense1 and weights2
+product2 = matmul(dense1, weights2)
+
+# Apply activation to product2 + bias2 and print the prediction
+prediction = keras.activations.sigmoid(product2+ bias2)
+print('\n prediction: {}'.format(prediction.numpy()[0,0]))
+print('\n actual: 1')
+
+#output
+prediction: 0.9525741338729858
+ actual: 1
+
+# Compute the product of borrower_features and weights1
+products1 = matmul(borrower_features, weights1)
+
+# Apply a sigmoid activation function to products1 + bias1
+dense1 = keras.activations.sigmoid( products1 + bias1)
+
+# Print the shapes of borrower_features, weights1, bias1, and dense1
+print('\n shape of borrower_features: ', borrower_features.shape)
+print('\n shape of weights1: ', weights1.shape)
+print('\n shape of bias1: ', bias1.shape)
+print('\n shape of dense1: ', dense1.shape)
+
+#output
+    shape of weights1:  (3, 2)
+    
+     shape of bias1:  (1,)
+    
+     shape of dense1:  (5, 2)
+
+
+#keras sample 2 activation networks
+# Define the first dense layer
+dense1 = keras.layers.Dense(7, activation='sigmoid')(borrower_features)
+
+# Define a dense layer with 3 output nodes
+dense2 = keras.layers.Dense(3, activation='sigmoid')(dense1)
+
+# Define a dense layer with 1 output node
+predictions = keras.layers.Dense(1, activation='sigmoid')(dense)
+
+# Print the shapes of dense1, dense2, and predictions
+print('\n shape of dense1: ', dense1.shape)
+print('\n shape of dense2: ', dense2.shape)
+print('\n shape of predictions: ', predictions.shape)
+
+# Define the first dense layer
+dense1 = keras.layers.Dense(7, activation='sigmoid')(borrower_features)
+
+# Define a dense layer with 3 output nodes
+dense2 = keras.layers.Dense(3, activation='sigmoid')(dense1)
+
+# Define a dense layer with 1 output node
+predictions = keras.layers.Dense(1, activation='sigmoid')(dense2)
+
+# Print the shapes of dense1, dense2, and predictions
+print('\n shape of dense1: ', dense1.shape)
+print('\n shape of dense2: ', dense2.shape)
+print('\n shape of predictions: ', predictions.shape)
+
+
+#ACTIVATION FUNCTION ON LINEAR AND NON-LINEAR STATES
