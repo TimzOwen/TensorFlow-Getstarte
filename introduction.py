728650456bee3b2524de0d14c5845e22fbf61a73
Timz Owen <ktimothy@kabarak.ac.ke>
2020-05-19 14:48:51 +0300

    Update introduction.py
    sign language Recap preditction

--- introduction.py
+++ introduction.py
@@ -782,3 +782,106 @@ model = tf.keras.Model(inputs=[model1_inputs, model2_inputs], outputs=merged)
 #compile the model
 model.compile('adam',loss='categorical_crossentropy')
 
+
+
+
+#RECAP
+# Define a Keras sequential model
+model = keras.Sequential()
+
+# Define the first dense layer
+model.add(keras.layers.Dense(16, activation='relu', input_shape=(784,)))
+
+# Define the second dense layer
+model.add(keras.layers.Dense(8, activation='relu'))
+
+# Define the output layer
+model.add(keras.layers.Dense(4, activation='softmax'))
+
+# Print the model architecture
+print(model.summary())
+
+#output
+Total params: 12,732
+Trainable params: 12,732
+Non-trainable params: 0
+__________________________
+
+#Compiling a sequential model
+# In this exercise, you will work towards classifying letters 
+# from the Sign Language MNIST dataset; however, you will adopt a 
+# different network architecture than what you used in the previous
+#  exercise. There will be fewer layers, but more nodes. You will 
+#  also apply dropout to prevent overfitting. Finally, you will
+#   compile the model to use the adam optimizer and the 
+# categorical_crossentropy loss. You will also use a method 
+# in keras to summarize your model's architecture
+
+# Define the first dense layer
+model.add(keras.layers.Dense(16, activation='sigmoid', input_shape=(784,)))
+
+# Apply dropout to the first layer's output
+model.add(keras.layers.Dropout(0.25))
+
+# Define the output layer
+model.add(keras.layers.Dense(4, activation='softmax'))
+
+# Compile the model
+model.compile('adam', loss='categorical_crossentropy')
+
+# Print a model summary
+print(model.summary())
+
+#output
+Total params: 12,628
+Trainable params: 12,628
+Non-trainable params: 0
+
+
+
+#Defining multiple input layers
+
+# In some cases, the sequential API will not be
+# sufficiently flexible to accommodate your
+# desired model architecture and you will need
+# to use the functional API instead. If, for instance,
+# you want to train two models with different architectures
+# jointly, you will need to use the functional API to do this. In this exercise,
+# we will see how to do this. We will also use the
+# .summary() method to examine the joint model's architecture
+
+# For model 1, pass the input layer to layer 1 and layer 1 to layer 2
+m1_layer1 = keras.layers.Dense(12, activation='sigmoid')(m1_inputs)
+m1_layer2 = keras.layers.Dense(4, activation='softmax')(m2_inputs)
+
+# For model 2, pass the input layer to layer 1 and layer 1 to layer 2
+m2_layer1 = keras.layers.Dense(12, activation='relu')(m2_inputs)
+m2_layer2 = keras.layers.Dense(4, activation='softmax')(m1_inputs)
+
+# Merge model outputs and define a functional model
+merged = keras.layers.add([m1_layer2, m2_layer2])
+model = keras.Model(inputs=[m1_inputs, m2_inputs], outputs=merged)
+
+# Print a model summary
+print(model.summary())
+
+#output expected 
+Model: "model"
+__________________________________________________________________________________________________
+Layer (type)                    Output Shape         Param #     Connected to                     
+==================================================================================================
+input_2 (InputLayer)            [(None, 784)]        0                                            
+__________________________________________________________________________________________________
+input_1 (InputLayer)            [(None, 784)]        0                                            
+__________________________________________________________________________________________________
+dense_1 (Dense)                 (None, 4)            3140        input_2[0][0]                    
+__________________________________________________________________________________________________
+dense_3 (Dense)                 (None, 4)            3140        input_1[0][0]                    
+__________________________________________________________________________________________________
+add (Add)                       (None, 4)            0           dense_1[0][0]                    
+                                                                 dense_3[0][0]                    
+==================================================================================================
+Total params: 6,280
+Trainable params: 6,280
+Non-trainable params: 0
+________________________
