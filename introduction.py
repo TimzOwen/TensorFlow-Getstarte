f22c234ab84bcee6bb94f9cdb846aecee8ef6c18
Timz Owen <ktimothy@kabarak.ac.ke>
2020-05-19 05:39:17 +0300

    Update introduction.py
    recap

--- introduction.py
+++ introduction.py
@@ -679,3 +679,53 @@ dropout1 = tf.keras.layers.Dropout(0.25)(dense2)
 outputs = tf.keras.layers.Dense(1, activation='sigmoid') (dropout1)
 
 
+
+#initialization Recap
+
+# Define the layer 1 weights
+w1 = Variable(random.normal([23, 7]))
+
+# Initialize the layer 1 bias
+b1 = Variable(ones([7]))
+
+# Define the layer 2 weights
+w2 = Variable(random.normal([7,1]))
+
+# Define the layer 2 bias
+b2 = Variable(0.0)
+
+
+# Define the model
+def model(w1, b1, w2, b2, features = borrower_features):
+	# Apply relu activation functions to layer 1
+	layer1 = keras.activations.relu(matmul(features, w1) + b1)
+    # Apply dropout
+	dropout = keras.layers.Dropout(0.25)(layer1)
+	return keras.activations.sigmoid(matmul(dropout, w2) + b2)
+
+# Define the loss function
+def loss_function(w1, b1, w2, b2, features = borrower_features, targets = default):
+	predictions = model(w1, b1, w2, b2)
+	# Pass targets and predictions to the cross entropy loss
+	return keras.losses.binary_crossentropy(targets, predictions)
+ 
+ #training neural networks
+ 
+ # Train the model
+for j in range(100):
+    # Complete the optimizer
+	opt.minimize(lambda: loss_function(w1, b1, w2, b2), 
+                 var_list=[w1, b1, w2, b2])
+
+# Make predictions with model
+model_predictions = model(w1, b1, w2, b2, test_features)
+
+# Construct the confusion matrix
+confusion_matrix(test_targets, model_predictions)
+
+
+
+
+#TRAINING AND CLASSIFYING  SIGN LANGUAGE NEURAL NETWORKS
+
+
