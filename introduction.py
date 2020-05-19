9d21cda8de61a83c7908d8a678964dddca154143
Timz Owen <ktimothy@kabarak.ac.ke>
2020-05-19 17:45:45 +0300

    Update introduction.py
    final commit and recap on estimators

--- introduction.py
+++ introduction.py
@@ -2332,6 +2332,42 @@ model1 =tf.estimator.DNNClassifier(feature_columns=feature_list,\
 
 #Train the classifier
 model1.train(input_fn, steps=20)
+		
+		
+#RECAP ON ESTIMATORS
+
+#For this exercise, we'll return to the King County housing 
+#transaction dataset from chapter 2. We will again
+#develop and train a machine learning model to predict house prices; 
+#however, this time, we'll do it using the estimator API.
+#Rather than completing everything in one step, we'll break this procedure down
+#into parts. We'll begin by defining the feature columns and loading the data.
+#In the next exercise, we'll define and train a premade estimator
+
+# Define feature columns for bedrooms and bathrooms
+bedrooms = feature_column.numeric_column("bedrooms")
+bathrooms = feature_column.numeric_column("bathrooms")
+
+# Define the list of feature columns
+feature_list = [bedrooms, bathrooms]
+
+def input_fn():
+	# Define the labels
+	labels = np.array(housing['price'])
+	# Define the features
+	features = {'bedrooms':np.array(housing['bedrooms']), 
+                'bathrooms':np.array(housing['bathrooms'])}
+	return features, labels
+
+
+#defining estimators
+# Define the model and set the number of steps
+model = estimator.DNNRegressor(feature_columns=feature_list, hidden_units=[2,2])
+model.train(input_fn, steps=1)
+
+#or use Linear Regressor
+model = estimator.LinearRegressor(feature_columns=feature_list)
+model.train(input_fn, steps=2)
 
 		
 		
