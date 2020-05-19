2563516530b8fe5bf12656eb21312d7e4b31f767
Timz Owen <ktimothy@kabarak.ac.ke>
2020-05-19 15:47:04 +0300

    Update introduction.py
    overfitting detection

--- introduction.py
+++ introduction.py
@@ -1085,4 +1085,25 @@ Epoch 10/10
 
 
 #OVERFITTING DETECTION
+# You will detect overfitting by checking whether the validation
+# sample loss is substantially higher than the training sample
+# loss and whether it increases with further training. With a
+# small sample and a high learning rate, the model will struggle
+# to converge on an optimum.You will set a low learning rate for the optimizer, which will
+#   make it easier to identify overfitting.
 
+# Define sequential model
+model = keras.Sequential()
+
+# Define the first layer
+model.add(keras.layers.Dense(1024, activation='relu', input_shape=(784,)))
+
+# Add activation function to classifier
+model.add(keras.layers.Dense(4, activation='softmax'))
+
+# Finish the model compilation
+model.compile(optimizer=keras.optimizers.Adam(lr=0.01), 
+              loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Complete the model fit operation
+model.fit(sign_language_features, sign_language_labels, epochs=200, validation_split=0.50)
